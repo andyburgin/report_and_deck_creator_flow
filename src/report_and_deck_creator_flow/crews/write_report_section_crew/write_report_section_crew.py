78d7e3391aa0ea2163b405eb01016bf219269fc1
root <github@andyburgin.co.uk>
2025-04-20 19:53:43 +0000

    Update report section writer crew

--- src/report_and_deck_creator_flow/crews/write_report_section_crew/write_report_section_crew.py
+++ src/report_and_deck_creator_flow/crews/write_report_section_crew/write_report_section_crew.py
@@ -1,62 +1,60 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import EXASearchTool
 
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from langchain_openai import ChatOpenAI
+import os
+
+from report_and_deck_creator_flow.types import ReportSection
+
 
 @CrewBase
 class WriteReportSectionCrew():
     """WriteReportSectionCrew crew"""
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
+    llm = ChatOpenAI(model="gpt-4o-mini")
 
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
     def researcher(self) -> Agent:
+        search_tool = EXASearchTool(api_key=os.getenv("EXA_API_KEY"))
+
         return Agent(
             config=self.agents_config['researcher'],
+            tools=[search_tool],
+            llm=self.llm,
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def writer(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'],
+            config=self.agents_config['writer'],
+            llm=self.llm,
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
-    def research_task(self) -> Task:
+    def research_report_section(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'],
+            config=self.tasks_config['research_report_section'],
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def write_report_section(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'],
-            output_file='report.md'
+            config=self.tasks_config['write_report_section'],
+            output_pydantic=ReportSection
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the WriteReportSectionCrew crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
